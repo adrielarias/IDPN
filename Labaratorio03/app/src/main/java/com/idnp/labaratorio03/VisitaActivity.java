9b8deee991ab42b781965755086ef229037bcaa0
DarkLobox <lgomezp@unsa.edu.pe>
2021-10-05 18:51:12 -0500

    VisitaActivity

---
+++ Labaratorio03/app/src/main/java/com/idnp/labaratorio03/VisitaActivity.java
@@ -0,0 +1,14 @@
+package com.idnp.labaratorio03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VisitaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visita);
+    }
+}
